7c561cfde6e39aba087f900a52e144c568b6815f
Alessandro Marchesin <a.marchesin@epsonemear.com>
2023-05-17 15:16:32 +0200

    First commit

--- src/services/dateService.js
+++ src/services/dateService.js
@@ -1,5 +1,25 @@
 const getCurrentDate = () => {
-    // Add your solution here!
+    // Create a new date object
+    let date = new Date();
+
+    // Get the day, month and year
+    let day = date.getDate();
+    let month = date.getMonth() + 1; // getMonth() returns 0-11
+    let year = date.getFullYear();
+
+    // Add leading zeros if needed
+    if (day < 10) {
+        day = "0" + day;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+
+    // Format the date as dd/mm/yyyy
+    let formattedDate = day + "/" + month + "/" + year;
+
+    // Log the result
+    return formattedDate // e.g. 17/05/2023
 };
 
 module.exports = {
